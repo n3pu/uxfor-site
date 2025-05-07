2b08c51b58ae2967d12a64663977a95110eaa8e6
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2025-05-07 19:06:19 -0300

    Ajuste mobile

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,13 @@ function App() {
             <img src={banner} className="banner-image" alt="Banner" />  
           </div>
         </div>
+        <p style={{width: '100%', textAlign: 'center'}}>Nos Siga:</p>
         <div className="social-media-buttons">
-          <SecondaryButton text="WhatsApp" onClick={() => window.open('', '_blank')} />
-          <SecondaryButton text="Telegram" onClick={() => window.open('', '_blank')} />
-          <SecondaryButton text="YouTube" onClick={() => window.open('', '_blank')} />
-          <SecondaryButton text="Instagram" onClick={() => window.open('', '_blank')} />
-          <SecondaryButton text="LinkedIn" onClick={() => window.open('', '_blank')} />
+          <SecondaryButton text="WhatsApp" onClick={() => window.open('https://chat.whatsapp.com/CbuGGYGIdxUD0F9bHr6wB7', '_blank')} />
+          <SecondaryButton text="Telegram" onClick={() => window.open('https://t.me/joinchat/Ed3KJ3qhxW1lZDFh', '_blank')} />
+          <SecondaryButton text="YouTube" onClick={() => window.open('http://www.youtube.com/@uxfor9309', '_blank')} />
+          <SecondaryButton text="Instagram" onClick={() => window.open('https://www.instagram.com/uxfor.br/', '_blank')} />
+          <SecondaryButton text="LinkedIn" onClick={() => window.open('https://www.linkedin.com/groups/9897026?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BB4aU8ypySCqgZeYgDq2VoA%3D%3D', '_blank')} />
         </div>
       </div>
       <Footer />
