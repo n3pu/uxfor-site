c6487fa158a6baa63162ea01c39b0723e8593981
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2025-05-09 10:59:10 -0300

    Clean branch

--- src/components/SecondaryButton.tsx
+++
@@ -1,7 +0,0 @@
-export function SecondaryButton({ text, onClick }: { text: string; onClick: () => void }) {
-  return (
-    <button className="secondary-button" onClick={onClick}>
-      {text}
-    </button>
-  );
-}
